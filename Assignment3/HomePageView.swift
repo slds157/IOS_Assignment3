37fc978df3646021329255ad14db3637c8aa04f8
lee <leegui230@gmail.com>
2023-05-14 02:15:08 +1000

    HomePage Branch 30% completed

--- Assignment3/HomePageView.swift
+++ Assignment3/HomePageView.swift
@@ -1,8 +1,34 @@
-//
-//  HomePageView.swift
-//  Assignment3
-//
-//  Created by 桂文杰 on 13/5/2023.
-//
+import UIKit
 
-import Foundation
+class HomePageViewController: UIViewController {
+
+    var scrollView: UIScrollView!
+    var images: [String] = ["image1", "image2", "image3", "image4", "image5"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupScrollView()
+    }
+
+    private func setupScrollView() {
+        let imageWidth = self.view.frame.size.width
+        let imageHeight = self.view.frame.size.height * 0.5
+
+        scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: imageWidth, height: imageHeight))
+        scrollView.isPagingEnabled = true
+        scrollView.contentSize = CGSize(width: imageWidth * CGFloat(images.count), height: imageHeight)
+        scrollView.showsHorizontalScrollIndicator = false
+       
+        for i in 0 ..< images.count {
+            let imageView = UIImageView(frame: CGRect(x: imageWidth * CGFloat(i), y: 0, width: imageWidth, height: imageHeight))
+            guard let image = UIImage(named: images[i]) else {
+                continue
+            }
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFit
+            scrollView.addSubview(imageView)
+        }
+
+        self.view.addSubview(scrollView)
+    }
+}
