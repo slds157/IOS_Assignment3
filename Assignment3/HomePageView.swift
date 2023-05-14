a3990f94363023b7a60187059751a9e99e432e3a
lee <leegui230@gmail.com>
2023-05-14 18:35:33 +1000

    homepage 40% completed

--- Assignment3/HomePageView.swift
+++ Assignment3/HomePageView.swift
@@ -1,16 +1,20 @@
 import UIKit
+import Foundation
 
 class HomePageViewController: UIViewController {
 
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var attentionLabel: UILabel!
     var scrollView: UIScrollView!
     var images: [String] = ["image1", "image2", "image3", "image4", "image5"]
-
+    var name: String?
+    var imageName: String?
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupScrollView()
+        setImageSliding()
     }
 
-    private func setupScrollView() {
+    private func setImageSliding() {
         let imageWidth = self.view.frame.size.width
         let imageHeight = self.view.frame.size.height * 0.5
 
@@ -31,4 +35,21 @@ class HomePageViewController: UIViewController {
 
         self.view.addSubview(scrollView)
     }
+    
+//    @objc private func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+//        let tappedImageView = tapGestureRecognizer.view as! UIImageView
+//        imageName = images[tappedImageView.tag]
+//        performSegue(withIdentifier: "goToDetails", sender: self)
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "goToGame", let name = nameText.text, !name.isEmpty {
+//            let VC = segue.destination as! movieDetails
+//           VC.imageName = imageName
+//            VC.name = name
+//            attentionLabel.text = ""
+//        }
+//        attentionLabel.text = "Please input your name!"
+//    }
+
 }
