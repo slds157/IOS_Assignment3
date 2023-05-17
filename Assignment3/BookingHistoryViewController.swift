d3c94e5a454df034b215f0107404f5764004b1bd
Zheng Yuan <zheng.yuan-1@student.uts.edu.au>
2023-05-17 14:57:57 +1000

    code update

--- Assignment3/BookingHistoryViewController.swift
+++ Assignment3/BookingHistoryViewController.swift
@@ -7,12 +7,53 @@
 
 import UIKit
 
-class BookingHistoryViewController: UIViewController {
+struct Booking: Codable {
+    let movieName: String
+    let userName: String
+}
 
+class BookingHistoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var bookings: [Booking] = [] // This will hold our booking data
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set the delegate and data source for the TableView
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Load the bookings
+        loadBookings()
+    }
+    
+    func loadBookings() {
+        // Load the bookings from UserDefaults
+        if let data = UserDefaults.standard.value(forKey:"bookings") as? Data {
+            bookings = try! PropertyListDecoder().decode(Array<Booking>.self, from: data)
+            tableView.reloadData()
+        }
     }
 
+    // MARK: - Table view data source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bookings.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookingHistoryCell", for: indexPath)
+        let booking = bookings[indexPath.row]
+        cell.textLabel?.text = booking.movieName
+        cell.detailTextLabel?.text = booking.userName
+        return cell
+    }
+    
+    
+    @IBAction func goHome(_ sender: UIBarButtonItem) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
 }
+
--- Assignment3/IssuedTicketsViewController.swift
+++ Assignment3/IssuedTicketsViewController.swift
@@ -7,12 +7,54 @@
 
 import UIKit
 
-class IssuedTicketsViewController: UIViewController {
+struct Ticket: Codable {
+    let seatNumber: String
+    let movieName: String
+    let session: String
+    let date: Date
+}
 
+class IssuedTicketsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tickets: [Ticket] = [] // This will hold our ticket data
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Set the delegate and data source for the TableView
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Load the tickets
+        loadTickets()
     }
-
-
+    
+    func loadTickets() {
+        // Load the tickets from UserDefaults
+        if let data = UserDefaults.standard.value(forKey:"tickets") as? Data {
+            tickets = try! PropertyListDecoder().decode(Array<Ticket>.self, from: data)
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: - Table view data source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tickets.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IssuedTicketCell", for: indexPath)
+        let ticket = tickets[indexPath.row]
+        cell.textLabel?.text = ticket.movieName
+        cell.detailTextLabel?.text = "Seat: \(ticket.seatNumber), Session: \(ticket.session), Date: \(ticket.date)"
+        return cell
+    }
+    
+    @IBAction func goHome(_ sender: UIBarButtonItem) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
 }
