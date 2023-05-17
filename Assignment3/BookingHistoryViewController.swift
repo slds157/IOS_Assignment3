8935d9aa651ca2523dbfe2489831cbfca63152e9
Zheng Yuan <zheng.yuan-1@student.uts.edu.au>
2023-05-17 11:06:17 +1000

    initial commit

---
+++ Assignment3/BookingHistoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BookingHistoryViewController.swift
+//  Assignment3
+//
+//  Created by Zheng Yuan on 2023-05-17.
+//
+
+import UIKit
+
+class BookingHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Assignment3/IssuedTicketsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  IssuedTicketsViewController.swift
+//  Assignment3
+//
+//  Created by Zheng Yuan on 2023-05-17.
+//
+
+import UIKit
+
+class IssuedTicketsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
